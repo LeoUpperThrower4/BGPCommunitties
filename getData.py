4b5a83166f75ac034e091939cf19da1fbf0be4d0
RafaelHuszcza <rafaelhuszcza@gmail.com>
2023-11-21 23:57:46 -0300

    feat: creating a get data script and fix: script to create a results.json per server

---
+++ getData.py
@@ -0,0 +1,59 @@
+import requests
+import json
+import time
+
+
+
+# Retorna a lista de todos os servidores
+def getRouterServerList():
+  response = requests.get('https://lg.ix.br/api/v1/routeservers')
+  if response.status_code == 200:
+    with open('routeservers.json', 'w') as f:
+      json.dump(json.loads(response.text), f)
+  else:
+    print('Error: ' + str(response.status_code))
+
+def getRouteServerNeighbors(serverId) -> list:
+  response = requests.get('https://lg.ix.br/api/v1/routeservers/'+serverId+'/neighbors')
+  if response.status_code == 200:
+    with open("neighbors-"+serverId+".json", 'w') as f:
+      json.dump({"neighbors": json.loads(response.text).get("neighbors")}, f)
+  else:
+    print('Error: ' + str(response.status_code))
+
+def getAnnouncedRoutes(serverId, neighborId, page):
+  response = requests.get('https://lg.ix.br/api/v1/routeservers/'+serverId+'/neighbors/'+neighborId+'/routes/received?page='+str(page))
+  if response.status_code == 200:
+      return json.loads(response.text)
+  else:
+    print('Error: ' + str(response.status_code))
+ 
+def getAllAnnouncedRoutesOfNeighbor(serverId, neighborId):
+  currentPage = 0
+  allAnnouncedRoutes = []
+  with open("announced-"+neighborId+".json", 'w') as f:
+    while True:
+      announcedRoutes = getAnnouncedRoutes(serverId, neighborId, currentPage)
+      if announcedRoutes.get('pagination').get('total_pages') == currentPage: break
+
+      if not(announcedRoutes.get('imported')): break
+      # se der problema no loop, sai na centesima pagina
+      if currentPage == 100 : break
+      allAnnouncedRoutes.extend(announcedRoutes.get('imported'))
+      currentPage += 1
+    json.dump({"announced":allAnnouncedRoutes}, f)
+
+if __name__ == '__main__':
+  # Se quiser usar aquivos X, mude os nomes e comente essa linha
+  getRouterServerList()
+  with open('routeservers.json', 'r') as f:
+    servers = json.load(f).get('routeservers')
+  for server in servers:
+    # Se quiser usar aquivos X, mude os nomes e comente essa linha
+    getRouteServerNeighbors(server.get('id'))
+    with open("neighbors-"+server.get('id')+".json", 'r') as f:
+      neighbors = json.load(f).get('neighbors')
+    for neighbor in neighbors:
+      getAllAnnouncedRoutesOfNeighbor(server.get('id'), neighbor.get('id'))
+      time.sleep(1)
+    time.sleep(1)
--- script.py
+++ script.py
@@ -1,8 +1,6 @@
-import requests
+
 import json
-import time
 # Lista de Comunidades de Ação BGP
-# TODO: atualizar com os valores corretos
 # Link: https://docs.ix.br/doc/communities-table-ix-br-v2_0-24112022.pdf
 actionBGPCommunitiesList = [
   65000,
@@ -24,59 +22,6 @@ actionBGPCommunitiesList = [
   64623
 ]
 
-# Retorna a lista de todos os servidores
-def getRouteServerList() -> list:
-  response = requests.get('https://lg.ix.br/api/v1/routeservers')
-  if response.status_code == 200:
-    return list(json.loads(response.text).get('routeservers'))
-  else:
-    return None
-
-# Retorna os neighbors do serverId
-def getRouteServerNeighbors(serverId) -> list:
-  response = requests.get('https://lg.ix.br/api/v1/routeservers/'+serverId+'/neighbors')
-  if response.status_code == 200:
-    return list(json.loads(response.text).get('neighbors'))
-  else:
-    return None
-
-# Retorna as rotas anunciadas pelo neighborId (na pagina especificada)
-def getAnnouncedRoutes(serverId, neighborId, page):
-  response = requests.get('https://lg.ix.br/api/v1/routeservers/'+serverId+'/neighbors/'+neighborId+'/routes/received?page='+str(page))
-  return json.loads(response.text)
-
-# Função usada para fazer a paginação das rotas anunciadas.
-# Retorna uma lista com todas as rotas anunciadas pelo neighborId
-def getAllAnnouncedRoutesOfNeighbor(serverId, neighborId) -> list:
-  allRoutes = []
-  currentPage = 0
-  print('Getting routes of neighbor ' + neighborId)
-  while True:
-    announcedRoutes = getAnnouncedRoutes(serverId, neighborId, currentPage)
-    allRoutes += list(announcedRoutes.get('imported'))
-    ## Condições de saída
-    # se for a ultima pagina
-    if announcedRoutes.get('pagination').get('total_pages') - 1 == currentPage: break
-    # se o campo 'imported' estiver vazio
-    if not(announcedRoutes.get('imported')): break
-    # se der problema no loop, sai na centesima pagina
-    
-    if currentPage == 100 : break
-    currentPage += 1
-
-  return allRoutes
-
-# Retorna uma lista com todas as rotas anunciadas por um servidor
-def getAllAnnouncedRoutesOfServer(serverId) -> list:
-  allRoutes = []
-  neighbors = getRouteServerNeighbors(serverId)
-  counter = 0
-  for neighbor in neighbors:
-    allRoutes += getAllAnnouncedRoutesOfNeighbor(serverId, neighbor.get('id'))
-    counter += 1
-    # if counter == 1: break # Limite para testes. Fica mais rapido e não consome toda a cota da API
-  return allRoutes
-
 def getAllActionBGPCommunitties(communitiesList) -> list:
   participatingCommunities = []
   for community in communitiesList:
@@ -84,26 +29,38 @@ def getAllActionBGPCommunitties(communitiesList) -> list:
       participatingCommunities.append(community[0])
   return participatingCommunities
 
-def displayInformation(neighborComunityDict):
-  for neighbor in neighborComunityDict:
-    print('Neighbor ID: ' + neighbor)
-    print('Participating Communities: ')
-    for community in neighborComunityDict[neighbor]:
-      print('  ' + str(community))
-    print('')
 
 if __name__ == '__main__':
-  # TODO: depois que o script estiver pronto, remover o limite de 3 neighbors e fazer um loop em todos os servidores usando getRouteServerList()
-  serverId = 'BEL-rs1-v4'
-  announcedRoutes = getAllAnnouncedRoutesOfServer(serverId)
-  neighborComunityDict = {}
-  for route in announcedRoutes:
-    communitiesList = list(route.get('bgp').get('communities'))
-    # TODO: devemos usar só communities ou large communities tambem?
-    # largeCommunitiesList = list(neighbor.get('bgp').get('large_communities'))
+  with open('routeservers.json', 'r') as serversFile:
+    servers = json.load(serversFile).get('routeservers')
+    for server in servers:
+      with open("neighbors-"+server.get('id')+".json", 'r') as neighborsFile:
+        neighbors = json.load(neighborsFile).get('neighbors')
+        neighbor_ids = []
+        announced_routes_per_neighbor = {}
+        announced_bgp_per_router_per_neighbor = {}
+        for neighbor in neighbors:
+          neighbor_ids.append(neighbor.get('id'))
+          with open("announced-"+neighbor.get('id')+".json", 'r') as announcedFile:
+            announcedRoutes = json.load(announcedFile).get('announced')
+            announced_routes_ids = []
+            announced_bgp_per_route = []
+            for route in announcedRoutes:
+              announced_routes_ids.append(route.get('id'))
+              communitiesList = list(route.get('bgp').get('communities'))
+              allRouteCommunities = getAllActionBGPCommunitties(communitiesList)
+              if len(allRouteCommunities) > 0:
+                announced_bgp_per_route.append({route.get('id') : allRouteCommunities})
+            announced_bgp_per_router_per_neighbor.update({neighbor.get('id') : announced_bgp_per_route})
+            announced_routes_per_neighbor.update({neighbor.get('id'): announced_routes_ids})
 
-    # TODO: devemos usar o ID da rota ou o ID do neighbor?
-    allRouteCommunities = getAllActionBGPCommunitties(communitiesList)
-    if allRouteCommunities:
-      neighborComunityDict[route.get('neighbor_id')] = allRouteCommunities
-  displayInformation(neighborComunityDict)
+      with open("results-"+server.get('id')+".json", 'w') as f:
+              results = {
+                "server_id": server.get('id'),
+                "neighbors_ids": neighbor_ids,
+                "announced_routes_per_neighbor": announced_routes_per_neighbor ,
+                "announced_bgp_per_router_per_neighbor": announced_bgp_per_router_per_neighbor
+              }
+              json.dump({"results": results}, f)
+
+
